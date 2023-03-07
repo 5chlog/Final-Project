1a9c6c19d2372942f04561e06f1f1c3996726011
VivekBM22 <vivekbm222gmail.com>
2023-03-07 13:32:46 +0530

    dialogs for level 1 integrated

--- Scripts/Mailman Conclusion.gd
+++ Scripts/Mailman Conclusion.gd
@@ -9,7 +9,6 @@ func _ready():
 	
 func interact():
 	DialogBox.enable_dialog_box( dialog_resource[0], self, $InteractableArea.player)
-	$InteractableArea.active = false
 
 #func sure_button():
 #	dialog_box.enable_dialog_box( dialog_resource[1], self, $InteractableArea.player)
