7a0f4a49dbb95e4cfe9d36ac4f57b8caafdda940
AshwinC8 <ashwinsam01@yahoo.com>
2023-03-16 12:41:44 +0530

    added the partsDisplay and scanner scene to conveyor testscene

---
+++ Mechanic Puzzle/Test/Parts_Display.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+enum partNames { GEAR, BOLT, SCREW, BATTERY }
+export(Array, partNames) var parts_used
+
+
+onready var partsDictionary:Dictionary = {
+	partNames.GEAR : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Gear.png")
+		],
+	partNames.BATTERY : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Battery.png")
+		],
+	partNames.BOLT : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Bolt.png")
+		],
+	partNames.SCREW : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Screw.png")
+		],
+}
+
+
+func _ready():
+	pass
