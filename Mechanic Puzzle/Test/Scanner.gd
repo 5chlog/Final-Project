771e3c9e47ebfc36c691691aee5b247dc533d32d
VivekBM22 <vivekbm222gmail.com>
2023-03-16 22:27:57 +0530

    Added Scanner-related sprites and added equip, unequip, show_parts and clear_parts functions to Scanner

--- Mechanic Puzzle/Test/Scanner.gd
+++ Mechanic Puzzle/Test/Scanner.gd
@@ -1,16 +1,59 @@
 extends CanvasLayer
 
+onready var screen: Sprite = get_child(0)
+onready var display = get_node("../Display")
+var parts_on_screen: int  = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var temp = 0 # Remove along with _physics_process
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen.frame = 1
+	visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Remove _physics_process after objects are completed in Conveyor Test.tscn
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if temp == 0:
+			equip()
+		elif temp == 1:
+			show_parts([display.partNames.SCREW, display.partNames.BATTERY])
+		elif temp == 2:
+			clear_parts()
+		temp += 1
+
+
+func equip():
+	visible = true
+	pass
+
+
+func unequip():
+	visible = false
+	pass
+
+
+func show_parts(var parts: Array):
+	parts_on_screen = 0
+	for part in parts:
+		var cell = screen.get_child(parts_on_screen)
+		# if display.partsDictionary[part][0]:
+		# Indent from here
+		cell.get_child(0).texture = display.partsDictionary[part][1]
+		cell.visible = true
+		parts_on_screen += 1
+		# Indent till here
+	
+	if parts_on_screen > 0:
+		screen.frame = 0
+
+
+func clear_parts():
+	if parts_on_screen == 0:
+		return
+	for i in parts_on_screen:
+		var cell = screen.get_child(i)
+		cell.visible = false
+	screen.frame = 1
+	parts_on_screen = 0
