cf985a3f550e36ba19f039c46730bd2d1fb83ea9
AshwinC8 <ashwinsam01@yahoo.com>
2023-03-20 17:04:09 +0530

    Added the parts display and also spaced out the pbjects in the conveyor

--- Mechanic Puzzle/Test/Parts_Display.gd
+++ Mechanic Puzzle/Test/Parts_Display.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name PartsDisplay
 
-enum partNames { GEAR, BOLT, SCREW, BATTERY }
+enum partNames { GEAR, BOLT, SCREW, BATTERY, MOTOR, BLADES, FUEL, METAL}
 export(Array, partNames) var parts_used
 
 
@@ -13,7 +13,7 @@ onready var partsDictionary:Dictionary = {
 		],
 	partNames.BATTERY : 
 		[
-			false,
+			true,
 			preload("res://Mechanic Puzzle/Test/Sprites/parts/Battery.png")
 		],
 	partNames.BOLT : 
@@ -26,8 +26,51 @@ onready var partsDictionary:Dictionary = {
 			false,
 			preload("res://Mechanic Puzzle/Test/Sprites/parts/Screw.png")
 		],
+	partNames.MOTOR : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Screw.png")
+		],
+	partNames.BLADES : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Screw.png")
+		],
+	partNames.FUEL : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Screw.png")
+		],
+	partNames.METAL : 
+		[
+			false,
+			preload("res://Mechanic Puzzle/Test/Sprites/parts/Screw.png")
+		],
 }
 
 
 func _ready():
+	var i:int = 0 
+	var n:int = get_child_count()
+	
+	checker()
+	
+	for part in parts_used:
+		if i<n:
+			var child = get_child(i)
+			print(child.get_child(0))
+			child.get_child(0).texture = partsDictionary[part][1]
+			i+=1
+	pass
+	
+func checker():
+	var i:int = 0 
+	var n:int = get_child_count()
+	
+	for part in parts_used:
+		if i<n:
+			if partsDictionary[part][0]:
+				get_child(i).frame = 1
+			i+=1
+		
 	pass
