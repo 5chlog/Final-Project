6272b2f64fbf5abd0df6a96e909da0fe33e87b1b
VivekBM22 <vivekbm222gmail.com>
2023-04-03 14:53:46 +0530

    Added basic mechanism to make gift follow player

--- Santa Claus/Gift.gd
+++ Santa Claus/Gift.gd
@@ -1,12 +1,24 @@
-extends Area2D
+extends Node2D
 
 onready var animation = $AnimationPlayer
 export(Texture) var sprite = preload("res://Santa Claus/Sprites/gift1.png")
+var is_picked_up = false
+var player
 
 func _ready():
 	$Sprite.texture = sprite
-	pass # Replace with function body.
-
-
-func _process(delta):
+	$Sprite.offset = -sprite.get_size() / 2
 	animation.play("Idle")
+
+
+func _physics_process(delta):
+	if is_picked_up:
+		position = player.position - Vector2(0, 32)
+
+func interact():
+	player = $InteractableArea.player
+	player.toggleHold()
+	$InteractableArea.disable()
+	position = player.position - Vector2(0, 32)
+	is_picked_up = true
+	get_parent().selected_gift = self
