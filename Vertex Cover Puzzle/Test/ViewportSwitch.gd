2b020bff7f74743121049e5a206f557d7bb81506
VivekBM22 <vivekbm222gmail.com>
2023-04-04 23:05:41 +0530

    Moved scrolling functionality to camera in map scene and bounded scrolling. Also modified the map background image and increased viewport size

--- Vertex Cover Puzzle/Test/ViewportSwitch.gd
+++ Vertex Cover Puzzle/Test/ViewportSwitch.gd
@@ -13,16 +13,17 @@ func interact():
 	
 	var player = interactactable_area.player
 	var control = get_node("../Control")
-	var background = control.get_node("ViewportContainer/Viewport/Test Map/Background")
-	background.player = player
-	background.switch = self
-	background.active = true
-	control.visible = true
+	var viewport_camera = control.get_node("ViewportContainer/Viewport/Test Map/Camera2D")
+	viewport_camera.player = player
+	viewport_camera.switch = self
+	viewport_camera.active = true
+	viewport_camera.visible = true
 	frame = 2
 	
 	var player_camera = player.get_node("Camera2D")
-	var viewport_node = control.get_node("ViewportContainer/Viewport")
-	var displacement = Vector2(viewport_node.size.x / 2, viewport_node.size.y / 2 
-			- player_camera.offset.y) 
-	var new_pos = -player.position + control.rect_position + displacement
+	var viewport_container = control.get_node("ViewportContainer")
+	var viewport_node = viewport_container.get_node("Viewport")
+	var displacement = Vector2(int(viewport_node.size.x / 2), int(viewport_node.size.y / 2))
+	var cam_offset = Vector2(0, int(player_camera.offset.y))
+	var new_pos = -player.position + control.rect_position + viewport_container.rect_position + displacement - cam_offset
 	player.get_node("Camera2D").position = new_pos
