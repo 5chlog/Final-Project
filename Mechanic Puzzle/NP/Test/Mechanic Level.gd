0b5b6142b7e1b16576e798c7cf0b8cf70acb3211
VivekBM22 <vivekbm222gmail.com>
2023-04-26 11:57:35 +0530

    Sorted parts_used before assigning parts to cell. Made new folder NP and moved Test and Sprites(from inside Test) to NP.

---
+++ Mechanic Puzzle/NP/Test/Mechanic Level.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+export(bool) var has_solution = true 
+
+var extra_hud_scene = preload("res://Mechanic Puzzle/NP/Test/Extra HUD.tscn")
+
+
+func _ready():
+	var extra_hud = extra_hud_scene.instance()
+	HUD.add_child(extra_hud)
+	HUD.get_node("Extra HUD/AnimationPlayer").connect("animation_finished", get_node("Mechanic"), 
+			"_scanner_anim_completed")
