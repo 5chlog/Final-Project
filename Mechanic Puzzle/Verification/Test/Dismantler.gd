32e4821f8a4444d613a96a1f3d87843ded052bf9
VivekBM22 <vivekbm222gmail.com>
2023-04-28 18:42:35 +0530

    Added script for PartBox and modified existong scripts to support PartBox. Added Dismantle animation.

--- Mechanic Puzzle/Verification/Test/Dismantler.gd
+++ Mechanic Puzzle/Verification/Test/Dismantler.gd
@@ -11,6 +11,7 @@ var machine_dismantled = []
 onready var prev_object = get_node("ObjectDisplay/Objects/PreviousObject")
 onready var cur_object = get_node("ObjectDisplay/Objects/CurrentObject")
 onready var next_object = get_node("ObjectDisplay/Objects/NextObject")
+onready var obj_dismantled_sprite = get_node("ObjectDisplay/CompletedSprite")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -43,14 +44,28 @@ func _input(_event):
 		active = false
 		$InteractableArea.enable()
 		$InteractableArea.player.toggleHold()
+		
 	elif Input.is_action_just_pressed("game_left") and machine_count > 1:
+		var prev_index = (index - 1 + machine_count) % machine_count
+		if machine_dismantled[index] and not machine_dismantled[prev_index]:
+			obj_dismantled_sprite.visible = false
+		
 		$AnimationPlayer.play("Move Display Right")
 		running = true
+		
 	elif Input.is_action_just_pressed("game_right") and machine_count > 1:
+		var next_index = (index + 1) % machine_count
+		if machine_dismantled[index] and not machine_dismantled[next_index]:
+			obj_dismantled_sprite.visible = false
+		
 		$AnimationPlayer.play("Move Display Left")
 		running = true
-	elif Input.is_action_just_pressed("ui_accept") and machine_count > 0:
-		pass
+		
+	elif Input.is_action_just_pressed("ui_accept") and machine_count > 0 and not machine_dismantled[index]:
+		$AnimationPlayer.play("Dismantle")
+		machine_dismantled[index] = true
+		obj_dismantled_sprite.visible = true
+		running = true
 
 
 func set_object_number_from_index(var object, var index: int):
@@ -63,10 +78,20 @@ func set_object_number_from_object(var object, var source_object):
 	object.get_node("LowerDigit").frame = source_object.get_node("LowerDigit").frame
 
 
+func set_boxes():
+	var parts_display = $"PartsDisplay"
+	var boxes = get_node("../Boxes")
+	for i in parts_display.parts_used.size():
+		if parts_display.get_child(i).frame == 1:
+			boxes.get_child(i).frame = 1
+
+
 func update_on_move(direction):
 	if direction == "Left":
 		index = (index + 1) % machine_count
 		$PartsDisplay.set_parts(parts_list[index])
+		if machine_dismantled[index]:
+			obj_dismantled_sprite.visible = true
 		
 		set_object_number_from_object(prev_object, cur_object)
 		set_object_number_from_object(cur_object, next_object)
@@ -74,6 +99,8 @@ func update_on_move(direction):
 	elif direction == "Right":
 		index = (index - 1 + machine_count) % machine_count
 		$PartsDisplay.set_parts(parts_list[index])
+		if machine_dismantled[index]:
+			obj_dismantled_sprite.visible = true
 		
 		set_object_number_from_object(next_object, cur_object)
 		set_object_number_from_object(cur_object, prev_object)
@@ -92,4 +119,6 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		
 		var prev_index = (index - 1 + machine_count) % machine_count
 		set_object_number_from_index(prev_object, prev_index)
+	elif anim_name == "Dismantle":
+		running = false
 		
