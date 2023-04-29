e8bfb1300dbd3341d1ee3f4504e9775cca44f6d4
VivekBM22 <vivekbm222gmail.com>
2023-04-29 13:37:56 +0530

    Changed Room sprite in NP. Removed set_input_as_handled for up and down cases and added game_back case in ViewportCamera script.

--- Buildng Powering Puzzle/NP/Test/ViewportCamera.gd
+++ Buildng Powering Puzzle/NP/Test/ViewportCamera.gd
@@ -23,18 +23,14 @@ func _input(event):
 		return
 	
 	if Input.is_action_just_pressed("game_up"):
-		get_tree().set_input_as_handled()
-		
 		# Move Camera up
 		if position.y > top_bound:
 			position.y -= UNIT_HEIGHT
 	elif Input.is_action_just_pressed("game_down"):
-		get_tree().set_input_as_handled()
-		
 		# Move Camera down
 		if position.y < bottom_bound:
 			position.y += UNIT_HEIGHT
-	elif Input.is_action_just_pressed("ui_accept"):
+	elif Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("game_back"):
 		get_tree().set_input_as_handled()
 		
 		# Return disable the viewport, enable InteractableArea of Switch and control to player
