2b89d49ce76f94f58841a0271f49c00b6ceaeb02
VivekBM22 <vivekbm222gmail.com>
2023-05-10 10:37:06 +0530

    Created Button functionality.

--- Glossary/LabelColumn.gd
+++ Glossary/LabelColumn.gd
@@ -4,7 +4,8 @@ export var _text = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = _text
+	#text = _text
+	pass
 
 #func _process(delta):
 #	pass
---
+++ Glossary/PageContainer.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+var current_page = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func set_current_page(var container):
+	current_page = container
